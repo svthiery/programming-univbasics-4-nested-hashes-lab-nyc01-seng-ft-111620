64754b5370f795e704cdd7a3aadd7903d5a7f4b2
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-10-28 16:46:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+  alans_new_info = "Cooking"
+  programmer_hash[:known_for] = alans_new_info
+  programmer_hash
 end
 
 def adding_to_dennis
