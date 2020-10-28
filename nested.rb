d61d91168d24f20f78a76e792980cd73b79f3941
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-10-28 16:46:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,7 @@ def changing_alan
       }
     }
   alans_new_info = "Cooking"
-  programmer_hash[:known_for] = alans_new_info
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
   programmer_hash
 end
 
